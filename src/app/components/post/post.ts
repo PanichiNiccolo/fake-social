05094bda6e36bb6c00a3e54f66e6fdad9baa4bf6
Niccolo Panichi <nikypanik98@gmail.com>
2025-10-11 10:21:10 +0200

    fixed component on test mode

--- src/app/components/post/post.ts
+++ src/app/components/post/post.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import {POSTS} from '../../data/posts';
 import {PostModel} from '../../models/post.model';
+import {Header} from '../header/header';
+import {CommentSection} from '../comment-section/comment-section';
 
 @Component({
   selector: 'app-post',
-  imports: [],
+  imports: [
+    Header,
+    CommentSection
+  ],
   templateUrl: './post.html',
   styleUrl: './post.css'
 })
