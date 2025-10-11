566ba64150fd9fd15867249a3c9f8547f00de3eb
Niccolo Panichi <nikypanik98@gmail.com>
2025-10-11 09:21:22 +0200

    implemented comment-section component

--- src/app/components/comment-section/comment-section.ts
+++ src/app/components/comment-section/comment-section.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {PostModel} from '../../models/post.model';
+import {POSTS} from '../../data/posts';
+import {Comment} from '../comment/comment';
 
 @Component({
   selector: 'app-comment-section',
-  imports: [],
+  imports: [
+    Comment
+  ],
   templateUrl: './comment-section.html',
   styleUrl: './comment-section.css'
 })
 export class CommentSection {
 
+  data: PostModel[] = POSTS;
+  singlePost: PostModel = POSTS[0];
 }
