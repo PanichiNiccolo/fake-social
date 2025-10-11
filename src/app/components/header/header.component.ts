c4a4566465d4252426c99159a779b0ae7a803c65
Niccolo Panichi <nikypanik98@gmail.com>
2025-10-11 08:16:47 +0200

    improved header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import {Post} from '../../models/post.model';
 import {POSTS} from '../../data/posts';
+import {NgOptimizedImage} from '@angular/common';
+import {AvatarComponent} from '../avatar/avatar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    AvatarComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
   data: Post[] = POSTS;
-  singlePost: Post = POSTS[0];
+  singlePost: Post = POSTS[1];
 }
