254e03d91abc01100aed58019618f815ea893b2b
Niccolo Panichi <nikypanik98@gmail.com>
2025-10-11 10:14:15 +0200

    implemented comment-section

--- src/app/components/comment-section/comment-section.ts
+++ src/app/components/comment-section/comment-section.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import {PostModel} from '../../models/post.model';
 import {POSTS} from '../../data/posts';
 import {Comment} from '../comment/comment';
+import {CommentUpdater} from '../comment-updater/comment-updater';
 
 @Component({
   selector: 'app-comment-section',
   imports: [
-    Comment
+    Comment,
+    CommentUpdater
   ],
   templateUrl: './comment-section.html',
   styleUrl: './comment-section.css'
